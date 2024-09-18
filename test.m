76e28b90ebf6463ee0a1359d74f5b3e7a00a2ec4
Bifbof <christof.leutenegger@hotmail.com>
2024-09-18 14:14:07 +0200

    add timeout handling

--- test.m
+++ test.m
@@ -1,6 +1,7 @@
 % Alternatively we could not print "output" and "expected"
 % This would make the whole thing more flexible for stuff like
 % functions that receive or return more complex values than doubles
+% (Matlab is special anyway in the way that a function can have multiple return values)
 
 % Instead of a function we can also test the output of a script in a similar manner
 
@@ -8,19 +9,28 @@ function test()
     % clc; % maybe clc is annyoing for the students
     print_header();
 
-    run_function(@() findRootByBisection(@(x) x, -1, 1), 0)
-    run_function(@() findRootByBisection(@(x) x, -1, 1), 1)
-    run_function(@() findRootByBisection(@(x) x, 1, 2), "error")
-    run_function(@() findRootByBisection(@(x) x, 1, 2), 0)
+    test_function(@() findRootByBisection(@(x) x, -1, 1), 0)
+    test_function(@() findRootByBisection(@(x) x, -1, 1), 1)
+    test_function(@() findRootByBisection(@(x) x, 1, 2), "error")
+    test_function(@() findRootByBisection(@(x) x, 1, 2), 0)
+    test_function(@() infinite_loop(), 0);
+end
+
+function answer = infinite_loop()
+    answer = 0; %#ok<NASGU>
+
+    while true
+    end
+
 end
 
 function print_header()
-    fprintf("         ");
+    fprintf("          ");
     fprintf("%-50s %-12s %-12s", "input", "output", "expected");
     fprintf("\n");
 end
 
-function run_function(f, expected, rel_tol, abs_tol)
+function test_function(f, expected, rel_tol, abs_tol)
     % Executes function and prints result to terminal
     arguments
         f (1, 1) function_handle
@@ -32,39 +42,36 @@ function run_function(f, expected, rel_tol, abs_tol)
     % Values of result:
     PASSED = 0; % function returned & correct result
     FAILED = 1; % function returned & false result
-    ERROR = 2; %  function raised an error
+    ERROR = 2; % function raised an error
+    TIMEOUT = 3; % function did not return on time
 
-    % if expected=="error", return PASSED if error was raised, otherwise FAILED
-    % we need to avoid ALL our errors in try-catch block -> do some extra checks
-    expects_error = false;
-    if isstring(expected) 
-        if expected == "error"
-            expects_error = true;
-        else
-            error('expected is a string but not "error"');
-        end
-    end
+    expects_error = isstring(expected) && expected == "error";
 
-    try
-        answer = f();
+    % values of status and result are shared
+    % PASSED and FINISHED are named differently tho
+    % I wish enums would not require an extra file :(
+    FINISHED = 0;
+    [answer, status] = run_function(f);
 
-        if expects_error
-            result = FAILED;
-        elseif isclose(answer, expected, rel_tol, abs_tol)
-            result = PASSED;
-        else
-            result = FAILED;
-        end
+    switch status
+        case TIMEOUT
+            result = TIMEOUT;
+        case ERROR
+            if expects_error
+                result = PASSED;
+            else
+                result = ERROR;
+            end
 
-    catch
-        answer = "error";
+        case FINISHED
+            if isclose(answer, expected, rel_tol, abs_tol)
+                result = PASSED;
+            else
+                result = FAILED;
+            end
 
-        if expects_error
-            result = PASSED;
-        else
-            result = ERROR;
-        end
-
+        otherwise
+            error("invalid status");
     end
 
     %% Print to output
@@ -72,24 +79,56 @@ function run_function(f, expected, rel_tol, abs_tol)
     % 1 is stdout
     % 2 is stderr
     % We (ab)use the fact that Matlab prints stderr red in terminal.
-    if result == PASSED
-        fprintf(1, "[PASSED] ");
-    elseif result == FAILED
-        fprintf(2, "[FAILED] ");
-    elseif result == ERROR
-        fprintf(2, "[ERROR]  ");
-    else
-        error("invalid result");
+    switch result
+        case PASSED
+            fprintf(1, "[PASSED]  ");
+        case FAILED
+            fprintf(2, "[FAILED]  ");
+        case ERROR
+            fprintf(2, "[ERROR]   ");
+        case TIMEOUT
+            fprintf(2, "[TIMEOUT] ");
+        otherwise
+            error("invalid result");
     end
 
     func = func2str(f);
-    % get rid of `@()` at start
-    func = func(4:end);
+    func = func(4:end); % get rid of `@()` at start
     answer = string(answer);
     expected = string(expected);
     fprintf("%-50s %-12s %-12s", func, answer, expected);
 
     fprintf("\n");
+
+end
+
+function [answer, status] = run_function(f)
+    % answer is return value of function
+    % status is one of the following {FINISHED, ERROR, TIMEOUT}
+    % if status = {ERROR, TIMEOUT} then answer is {"error", "timeout"} respectively
+    FINISHED = 0;
+    ERROR = 2;
+    TIMEOUT = 3;
+    % time to wait [s] for future to finish
+    timeout_time = 0.5;
+
+    try
+        fut = parfeval(backgroundPool, f, 1);
+        ok = fut.wait('finished', timeout_time);
+
+        if ok
+            answer = fetchOutputs(fut);
+            status = FINISHED;
+        else
+            cancel(fut);
+            answer = "timeout";
+            status = TIMEOUT;
+        end
+
+    catch
+        answer = "error";
+        status = ERROR;
+    end
 
 end
 
